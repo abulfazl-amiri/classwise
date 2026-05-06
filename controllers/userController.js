1d4f984ac346c2a9580e0cde54a05e586f971af6
Amiri <0xamiridev@gmail.com>
2026-05-06 22:30:38 +0430

    feat: added signup functionality with email existence check and error handling, add bcrypt and jsonwebtoken dependencies

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,19 +1,35 @@
-const signup = function (req, res) {
+import User from "../models/User.js";
+import bcrypt from "bcrypt";
+
+const signup = async function (req, res) {
   try {
-    let user;
+    if (Array.isArray(req.body)) {
+      throw new Error("Multi account creation was detected");
+    }
     // get the data
     const { username, password, email } = req.body;
 
+    const foundEmails = await User.find({ email: email });
+
+    // if user exist
+    if (foundEmails.length !== 0) {
+      throw new Error(`Email is already in use`);
+    }
+
+    // hashin the passwords
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const user = await User.create({ username: username, email: email, password: hashedPassword });
     res.status(200).json({
       status: "success",
       data: {
-        message: "Under Maintainance",
+        message: user,
       },
     });
   } catch (err) {
     res.status(400).json({
       status: "fail",
-      message: err,
+      message: err.message,
     });
   }
 };
