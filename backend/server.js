693ee73d328f37de23df490f8b87eb69168cec75
Amiri <0xamiridev@gmail.com>
2026-06-02 18:08:36 +0430

    chore: remove the local host url (127.0.0.1) from the server.js

--- backend/server.js
+++ backend/server.js
@@ -11,4 +11,4 @@ try {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, "127.0.0.1", () => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
